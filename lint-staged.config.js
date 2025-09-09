c5f8732e60410e9b1a8e61558e837be50947032e
Triska Mahfud K <triska.triskacode@gmail.com>
2025-09-09 22:39:22 +0700

    chore: update deps & change scripts

--- lint-staged.config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  '*.{ts,tsx}': ['pnpm lint:prettier', 'pnpm lint:eslint', 'pnpm test:types'],
-  '*.{js,json,yml,yaml,md,html,css}': ['pnpm lint:prettier', 'pnpm lint:eslint'],
-}
