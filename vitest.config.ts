021c99aba25fb55dec24eec1393b12820ff036f8
Triska Mahfud K <im.triskuy@gmail.com>
2026-01-02 22:11:47 +0700

    chore: upgrade vitest v4

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true,
+    name: 'unit',
     environment: 'node',
     include: ['test/**/*.test.ts'],
     coverage: {
+      enabled: true,
       provider: 'v8',
-      reporter: ['text', 'html'],
     },
   },
 })
